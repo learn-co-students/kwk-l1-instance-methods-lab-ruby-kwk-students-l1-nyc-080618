9a1f7b2b1f17fe7e119c9396b33f10e8bb03d6b1
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-09 19:17:42 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # dog.rb
+class Dog
+  def bark
+    puts "Woof!"
+  end 
+  def sit
+    puts "The Dog is sitting"
+  end 
+end
+
+
+# #### 2. Define `#bark` in `Dog`
+
+# Add an instance method `#bark` to your `Dog` class in `lib/dog.rb` that will puts `"Woof!"`
+
+# #### 3. Define `#sit` in `Dog`
+
+# Add an instance method `#sit` to your `Dog` class in `lib/dog.rb` that will puts `"The Dog is sitting"`.
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,13 @@
 # person.rb
+class Person
+  def talk
+    puts "Hello World!"
+  end
+  def walk 
+    puts "The Person is walking"
+  end
+end
+
+#example of a person (instance) would produce the same results of Hello World and Person is walking based on if they walk or talk 
+sophia = Person.new 
+sophia.talk
